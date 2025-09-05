beab69239a1a6ab8566d2787b8935629e32a284c
Hanif <widagdohanif@gmail.com>
2025-09-05 18:48:41 +0700

    add s3 backend

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.0"
-  
+  backend "s3" {
+    bucket = "project-iac-bucket"
+    key    = "terraform/terraform.tfstate"
+    region = "us-west-2"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
