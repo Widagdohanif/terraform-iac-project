4cfcf86bd9369d9b049eb1b4de34d27807745c58
Hanif <widagdohanif@gmail.com>
2025-09-06 03:33:11 +0700

    fix bash deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,16 +5,9 @@ set -e
 PROJECT_ROOT=$(dirname "$(dirname "$(realpath "$0")")")
 cd "$PROJECT_ROOT"
 
-echo "🚀 Starting full deployment..."
-
-# Build and push Flask app
-echo "🔨 Build and Push Flask application..."
-cd app/script
-./build.sh
-
 # Deploy infrastructure
 echo "📡 Deploying EKS infrastructure..."
-cd ../../iac
+cd ~/Documents/project-iac/terraform-iac/iac
 terraform init
 terraform plan -out=tfplan
 terraform apply tfplan
@@ -22,7 +15,7 @@ terraform apply tfplan
 # Get cluster config
 echo "🔧 Configuring kubectl..."
 CLUSTER_NAME=$(terraform output -raw cluster_name)
-AWS_REGION=$(terraform output -raw aws_region)
+AWS_REGION="us-east-1"
 aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 
 # Wait for cluster to be ready
@@ -31,7 +24,7 @@ kubectl wait --for=condition=Ready nodes --all --timeout=300s
 
 # Deploy Kubernetes resources
 echo "🎯 Deploying Kubernetes resources..."
-cd ../k8s/tf
+cd ~/Documents/project-iac/terraform-iac/k8s/tf
 terraform init
 terraform plan -out=tfplan
 terraform apply tfplan
@@ -48,4 +41,4 @@ echo "Cluster Name: $CLUSTER_NAME"
 echo "Flask App: kubectl port-forward svc/flask-app-service 8080:80"
 echo "Nginx LB: kubectl port-forward svc/nginx-loadbalancer-service 8090:80" 
 echo "Grafana: kubectl port-forward svc/prometheus-grafana -n monitoring 3000:80"
-echo "Grafana Password: $(cd ../tf && terraform output grafana_password)"
+echo "Grafana Password: $(cd ~/Documents/project-iac/terraform-iac/k8s/tf && terraform output grafana_password)"
